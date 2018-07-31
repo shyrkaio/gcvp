8c429b57e6c2ae651ab44c5f44690d1ee3bea800
Charles Sabourdin <kanedafromparis@gmail.com>
2018-07-31 15:54:31 +0200

    Developp Ephyra (pod killer)

---
+++ src/main/java/io/github/kandefromparis/shyrka/gcvp/Ephyra.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.kandefromparis.shyrka.gcvp;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.extensions.Deployment;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.openshift.api.model.DeploymentConfig;
+import io.fabric8.openshift.client.OpenShiftClient;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import io.vertx.core.json.JsonObject;
+import java.time.ZonedDateTime;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author csabourdin
+ */
+public class Ephyra {
+
+    private final Logger logger = LoggerFactory.getLogger(Ephyra.class);
+
+    public JsonObject load(String confPath) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper(new YAMLFactory()); // jackson databind
+        JsonNode root = mapper.readValue(new File(confPath), JsonNode.class);
+        return new JsonObject(root.toString());
+    }
+
+    public Boolean terminateOldPodOpenshift(JsonObject conf, OpenShiftClient osClient, String namespace, String label) {
+        Iterator<DeploymentConfig> iterator;
+        if (StringUtils.isBlank(label)) {
+            iterator = osClient.deploymentConfigs().inNamespace(namespace)
+                    .list().getItems()
+                    .iterator();
+        } else {
+            String key = StringUtils.split(label, "=")[0];
+            String value = StringUtils.split(label, "=")[1];
+            logger.warn("Filter on Label in working only with one label. we use '" + key + "=" + value+"'");
+            iterator = osClient.deploymentConfigs().inNamespace(namespace)
+                    .withLabel(key, value)
+                    .list().getItems()
+                    .iterator();
+
+        }
+        while (iterator.hasNext()) {
+            DeploymentConfig dc = iterator.next();
+            logger.info("dc : " + dc.getMetadata().getName());
+            if (dc.getSpec().getReplicas() > 0) {
+                Map<String, String> labels = new HashMap<>();
+                labels.put("deploymentconfig", dc.getMetadata().getName());
+                //This unfortunalty do not works with Mock
+                Iterator<Pod> pods = osClient.pods().withLabels(labels).list().getItems().iterator();
+                deletePods(conf, pods, osClient);
+            }
+        }
+
+        return Boolean.TRUE;
+    }
+
+    public Boolean terminateOldPod(JsonObject conf, KubernetesClient osClient, String namespace, String label) {
+        Iterator<Deployment> iterator;
+        if (StringUtils.isBlank(label)) {
+            iterator = osClient.extensions().deployments().inNamespace(namespace)
+                    .list().getItems()
+                    .iterator();
+        } else {
+            String key = StringUtils.split(label, "=")[0];
+            String value = StringUtils.split(label, "=")[1];
+            logger.warn("Filter on Label in working only with one label. we use " + key + "=" + value);
+            iterator = osClient.extensions().deployments().inNamespace(namespace)
+                    .withLabel(key, value).list().getItems()
+                    .iterator();
+
+        }
+
+        while (iterator.hasNext()) {
+            Deployment dc = iterator.next();
+            if (dc.getSpec().getReplicas() > 1) {
+                Map<String, String> labels = new HashMap<>();
+                labels.put("deployment", dc.getMetadata().getName());
+                Iterator<Pod> pods = osClient.pods().withLabels(labels).list().getItems().iterator();
+                deletePods(conf, pods, osClient);
+            }
+        }
+
+        return Boolean.TRUE;
+    }
+
+    private void deletePods(JsonObject conf, Iterator<Pod> pods, KubernetesClient osClient) {
+        int maxDuration = conf.getInteger("maxDuration", 5);
+        int nbPodCleaned = 0;
+        if (logger.isInfoEnabled()) {
+            logger.info("maxDuration : " + maxDuration);
+        }
+
+        while (pods.hasNext()) {
+            Pod next = pods.next();
+            if (StringUtils.equals(next.getStatus().getPhase(), "Running")) {
+                if (logger.isInfoEnabled()) {
+                    logger.info("Pod : " + next.getMetadata().getName() + " started at  " + next.getStatus().getStartTime());
+                }
+                if (nbPodCleaned++ > conf.getInteger("nbPodsToClean", 0)) {
+                    logger.info("nbPodCleaned : " + nbPodCleaned + " nbPodsToClean : " + conf.getInteger("nbPodsToClean", 0));
+                    return;
+                }
+                ZonedDateTime now = ZonedDateTime.now();
+                ZonedDateTime newDate = ZonedDateTime.parse(next.getStatus().getStartTime());
+                if (Duration.between(newDate, now).toMinutes() > maxDuration) {
+                    if (logger.isInfoEnabled()) {
+                        logger.info("Duration : " + Duration.between(newDate, now).toMinutes() + " deleting pod");
+                    }
+                    Boolean delete = osClient.pods().delete(next);
+                    if (logger.isInfoEnabled()) {
+                        logger.info("Pod : " + next.getMetadata().getName() + " deleted :" + delete);
+                    }
+                } else {
+                    if (logger.isInfoEnabled()) {
+                        logger.info("Duration : " + Duration.between(now, newDate).toDays() + " not deleting pod");
+                    }
+                }
+            }
+
+        }
+    }
+
+}
---
+++ src/test/java/io/github/kandefromparis/shyrka/gcvp/EphyraTest.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.kandefromparis.shyrka.gcvp;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.openshift.client.NamespacedOpenShiftClient;
+import io.fabric8.openshift.client.server.mock.OpenShiftServer;
+import io.vertx.core.json.JsonObject;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author csabourdin
+ */
+public class EphyraTest {
+
+    public EphyraTest() {
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(CaiusPupusTest.class);
+
+    @Rule
+    public OpenShiftServer ocpServer = new OpenShiftServer(true, true);
+    JsonObject conf = new JsonObject().put("maxDuration", 2);
+
+    Utils tools = new Utils();
+    final String NS = "groumphfs";
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+
+        this.ocpServer = tools.createFakeServer(ocpServer, NS);
+
+    }
+
+    @After
+    public void cleanUp() throws IOException {
+        this.ocpServer.getMockServer().close();
+    }
+
+    /**
+     * Test of load method, of class Ephyra.
+     */
+    @Test
+    @Ignore
+    //@DisabledIfSystemProperty(named = "ci-server", matches = "true")
+
+    public void testLoad() throws Exception {
+        System.out.println("load");
+        String confPath = "";
+        Ephyra instance = new Ephyra();
+        JsonObject expResult = null;
+        JsonObject result = instance.load(confPath);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of terminateOldPodOpenshift method, of class Ephyra.
+     */
+    @Test
+    public void testTerminateOldPodOpenshift() {
+        System.out.println("terminateOldPodOpenshift");
+        NamespacedOpenShiftClient client = ocpServer.getOpenshiftClient().inNamespace(NS);
+
+        Ephyra instance = new Ephyra();
+        Boolean expResult = Boolean.TRUE;
+        Boolean result = instance.terminateOldPodOpenshift(conf, client, NS, StringUtils.EMPTY);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of terminateOldPodOpenshift method, of class Ephyra.
+     */
+    @Test
+    public void testDate() {
+
+        int maxDuration = 5;
+        ZonedDateTime now = ZonedDateTime.now();
+
+        ZonedDateTime newDate000 = ZonedDateTime.parse("2018-07-25T18:58:44Z");
+        
+        ZonedDateTime newDate001 = ZonedDateTime.parse("2018-07-25T18:52:44Z");
+        ZonedDateTime newDate002 = ZonedDateTime.parse("2018-07-25T18:54:44Z");
+
+        assertTrue(newDate001.isBefore(now));
+        
+        assertEquals(Duration.between(newDate001, newDate000).toMinutes(),  6);
+        
+        assertTrue(Duration.between(newDate001,newDate000).toMinutes() > maxDuration);
+        
+        assertEquals(Duration.between(newDate002, newDate000).toMinutes(), 4);
+        assertFalse(Duration.between(newDate002, newDate000).toMinutes() > maxDuration);
+    }
+
+    /**
+     * Test of terminateOldPod method, of class Ephyra.
+     */
+    @Test
+    public void testTerminateOldPod() {
+        System.out.println("terminateOldPod");
+
+        KubernetesClient osClient = ocpServer.getKubernetesClient();
+
+        Ephyra instance = new Ephyra();
+        Boolean expResult = Boolean.TRUE;
+        Boolean result = instance.terminateOldPod(conf, osClient, NS,StringUtils.EMPTY);
+        assertEquals(expResult, result);
+    }
+
+}
